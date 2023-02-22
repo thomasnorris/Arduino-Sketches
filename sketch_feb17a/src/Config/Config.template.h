07510b45e8c554e61b2faefccc896901ad1930ed
Thomas Norris <tom.norris135@gmail.com>
2023-02-22 12:11:18 -0500

    changed wifi default led

--- sketch_feb17a/src/Config/Config.template.h
+++ sketch_feb17a/src/Config/Config.template.h
@@ -8,7 +8,7 @@ inline const int LOOP_DELAY_MS = 1000;
 // wifi
 inline const String WIFI_SSID = "";
 inline const String WIFI_PASS = "";
-inline const byte WIFI_LED_PIN = 16;
+inline const byte WIFI_LED_PIN = D4; // D4 is ESP8266 led, D0 is NodeMCU led
 inline const byte WIFI_LED_ON_VALUE = LOW;
 
 #endif
